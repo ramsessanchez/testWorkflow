6c029b8f0f2f57f1fe277dbf0308832c0c9001c6
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2021-09-21 02:26:01 -0700

    Update getLatestVersion.ps1

--- scripts/getLatestVersion.ps1
+++ scripts/getLatestVersion.ps1
@@ -14,4 +14,4 @@ $patchVersion = $content[$lineNumber].Substring($versionIndex)
 $version = "$majorVersion.$minorVersion.$patchVersion"
 
 #Set Task output to create tag
-Write-Output "::set-output name=tag::v${version}"
+Write-Output "::set-output name=tag::${version}"
