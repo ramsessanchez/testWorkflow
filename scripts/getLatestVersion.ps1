7019f79176ab87a0d228f3b73bbd66ed208a4321
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2021-09-21 02:04:12 -0700

    Create getLatestVersion.ps1

---
+++ scripts/getLatestVersion.ps1
@@ -0,0 +1,17 @@
+$propertiesPath = Join-Path -Path $PSScriptRoot -ChildPath "../gradle.properties"
+$file = get-item $propertiesPath
+$findVersions = $file | Select-String -Pattern "mavenMajorVersion" -Context 0,2
+$content = Get-Content $propertiesPath
+
+$lineNumber = $findVersions.LineNumber - 1
+$versionIndex = $content[$lineNumber].IndexOf("=")
+$versionIndex += 2 # skipping =[space]
+$majorVersion = $content[$lineNumber].Substring($versionIndex)
+$lineNumber++
+$minorVersion = $content[$lineNumber].Substring($versionIndex)
+$lineNumber++
+$patchVersion = $content[$lineNumber].Substring($versionIndex)
+$version = "$majorVersion.$minorVersion.$patchVersion"
+
+#Set Task output to create tag
+Write-Output "::set-output name=tag::v${version}"
