735b9dafd6a93fcdb585cb481741283ded37f34b
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2021-09-21 15:51:34 -0700

    better

--- scripts/compare-files.py
+++ scripts/compare-files.py
@@ -4,7 +4,12 @@ import sys
 dir1 = sys.argv[1]
 dir2 = sys.argv[2]
 
-filecmp.cmpfiles(dir1, dir2, shallow= False)
+common = ["hello.txt", "world.txt"]
 
+match, mismatch, errors = filecmp.cmpfiles(dir1, dir2, common, shallow= False)
 
+print("Deep Comparison")
+print("Match: ", match)
+print("Mismatch: ", mismatch)
+print("Errors: ", errors)
 
